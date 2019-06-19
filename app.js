681ad1c210498156a52af056fe642c1cde890e68
Mitch Denny <midenn@microsoft.com>
2019-06-19 00:02:43 +0000

    Tailing builds in public.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-setInterval(() => { console.log("Hello, World"); }, 1000);
+var sb = require("@azure/service-bus");
+
+const connectionString = process.env.SB_CONNECTION_STRING;
+
+const client = sb.ServiceBusClient.createFromConnectionString(connectionString);
+const queue = client.createQueueClient("build-completed");
+const receiver = queue.createReceiver(sb.ReceiveMode.peekLock);
+
+receiver.registerMessageHandler(
+    (message) => {
+        try {
+            const resource = message.body.resource;
+            const definition = resource.definition;
+            const id = resource.id;
+            console.log(`${resource.status}: ${definition.name} / ${id} (${resource.sourceGetVersion})`);
+        } catch {
+            console.log("Boom!");
+        }
+    },
+    (error) => {
+        console.log(error);
+        receiver.close();
+    }
+);
